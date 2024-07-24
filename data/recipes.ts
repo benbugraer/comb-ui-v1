bffd67926b3535df3ae79438d5e30b0bc5131871
benbugraer <bugraerdev@gmail.com>
2024-07-24 15:51:37 +0300

    DockWithPreview Recipe Content Update & For Code Blocks Figcaption & Some Style Changes

--- data/recipes.ts
+++ data/recipes.ts
@@ -4,69 +4,69 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import { unified } from "unified";
+import {unified} from "unified";
 import matter from "gray-matter";
-import { transformerCopyButton } from "@rehype-pretty/transformers";
-import { transformerNotationDiff } from "@shikijs/transformers";
+import {transformerCopyButton} from "@rehype-pretty/transformers";
+import {transformerNotationDiff} from "@shikijs/transformers";
 
 type Metadata = {
-  title: string;
-  description: string;
-  image?: string;
+    title: string;
+    description: string;
+    image?: string;
 };
 
 function getMDXFiles(dir: string) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+    return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
 }
 
 export async function markdownToHTML(markdown: string) {
-  const p = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypePrettyCode, {
-      transformers: [
-        transformerNotationDiff(),
-        transformerCopyButton({
-          visibility: "hover",
-          feedbackDuration: 1500,
-        }),
-      ],
-      theme: "one-light",
-      filterMetaString: (string) => string.replace(/filename="[^"]*"/, ""),
-    })
-    .use(rehypeStringify)
-    .process(markdown);
+    const p = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypePrettyCode, {
+            transformers: [
+                transformerNotationDiff(),
+                transformerCopyButton({
+                    visibility: "hover",
+                    feedbackDuration: 1500,
+                }),
+            ],
+            theme: "one-light",
+            filterMetaString: (string) => string.replace(/filename="[^"]*"/, ""),
+        })
+        .use(rehypeStringify)
+        .process(markdown);
 
-  return p.toString();
+    return p.toString();
 }
 
 export async function getRecipes(slug: string) {
-  const filePath = path.join("content", "recipes", `${slug}.mdx`);
-  let source = fs.readFileSync(filePath, "utf-8");
-  const { content: rawContent, data: metadata } = matter(source);
-  const content = await markdownToHTML(rawContent);
-  return {
-    source: content,
-    metadata,
-    slug,
-  };
+    const filePath = path.join("content", "recipes", `${slug}.mdx`);
+    let source = fs.readFileSync(filePath, "utf-8");
+    const {content: rawContent, data: metadata} = matter(source);
+    const content = await markdownToHTML(rawContent);
+    return {
+        source: content,
+        metadata,
+        slug,
+    };
 }
 
 async function getAllRecipes(dir: string) {
-  let mdxFiles = getMDXFiles(dir);
-  return Promise.all(
-    mdxFiles.map(async (file) => {
-      let slug = path.basename(file, path.extname(file));
-      let { metadata, source } = await getRecipes(slug);
-      return {
-        metadata,
-        slug,
-        source,
-      };
-    }),
-  );
+    let mdxFiles = getMDXFiles(dir);
+    return Promise.all(
+        mdxFiles.map(async (file) => {
+            let slug = path.basename(file, path.extname(file));
+            let {metadata, source} = await getRecipes(slug);
+            return {
+                metadata,
+                slug,
+                source,
+            };
+        }),
+    );
 }
 
 export async function getRecipePosts() {
-  return getAllRecipes(path.join(process.cwd(), "content", "recipes"));
+    return getAllRecipes(path.join(process.cwd(), "content", "recipes"));
 }
